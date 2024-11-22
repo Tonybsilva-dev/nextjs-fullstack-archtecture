05a69f303a9e4683726b75fae52242c42ffcd8b3
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2024-11-22 00:02:11 -0300

    chore: add version application constant

--- src/shared/modules/constants/application.constants.ts
+++ src/shared/modules/constants/application.constants.ts
@@ -1,7 +1,12 @@
-import { formatDate } from "../utils/format-date"
+import { formatDate } from '../utils/format-date';
+
+/* Version Application */
+export const VERSION_APPLICATION = `0.0.1`;
 
 /* Name Application */
-export const NAME_APPLICATION = `Fullstack Archtecture`
+export const NAME_APPLICATION = `Fullstack Archtecture`;
 
 /* Last Updated Application */
-export const LAST_UPDATED_APPLICATION = formatDate(new Date('Thu Nov 21 2024 22:53:05 GMT-0300'))
+export const LAST_UPDATED_APPLICATION = formatDate(
+  new Date('Thu Nov 21 2024 22:53:05 GMT-0300')
+);
