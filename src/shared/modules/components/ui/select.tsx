d1c21c77170ec1e80db999c37fba0c5b8ac86043
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2024-11-23 12:09:45 -0300

    fix: configuration shadcn-ui on import utils cn

--- src/shared/modules/components/ui/select.tsx
+++ src/shared/modules/components/ui/select.tsx
@@ -4,7 +4,7 @@ import * as SelectPrimitive from '@radix-ui/react-select';
 import { Check, ChevronDown, ChevronUp } from 'lucide-react';
 import * as React from 'react';
 
-import { cn } from '@/shared/modules/utils/shadcn.ts';
+import { cn } from '@/shared/modules/utils/shadcn';
 
 const Select = SelectPrimitive.Root;
 
--- src/shared/modules/components/ui/sheet.tsx
+++ src/shared/modules/components/ui/sheet.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { X } from 'lucide-react';
 import * as React from 'react';
 
-import { cn } from '@/shared/modules/utils/shadcn.ts';
+import { cn } from '@/shared/modules/utils/shadcn';
 
 const Sheet = SheetPrimitive.Root;
 
